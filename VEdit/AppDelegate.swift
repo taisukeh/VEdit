d6d59ea87d38ae6339c4a1c4d31cee44cbb15ea8
Taisuke Hori <taisuke.hori@gmail.com>
2017-12-24 23:31:36 +0900

    Initial Commit

---
+++ VEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VEdit
+//
+//  Created by 堀泰祐 on 2017/12/24.
+//  Copyright © 2017 taisukeh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ VEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VEdit
+//
+//  Created by 堀泰祐 on 2017/12/24.
+//  Copyright © 2017 taisukeh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
